f8ab353fc6168223e96c042103513cdeb9064728
Shunya Sago <shun.s0029@gmail.com>
2021-09-16 13:17:13 +0000

    [Add] admin/homes/top

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
-  enum is_active:{ 販売中: '販売中', 販売停止中: '販売停止中' }
+  enum is_active: {
+    販売中: '販売中',
+    販売停止中: '販売停止中'
+  }
   belongs_to :genre
   attachment :image
 end
