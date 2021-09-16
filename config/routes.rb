08cded74b625bd9bf6dad2d3d36e166336acdee0
Shunya Sago <shun.s0029@gmail.com>
2021-09-16 13:32:17 +0000

    [Start] admin/注文履歴詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'homes/top'
+    get 'orders/show'
   end
   devise_for :admin_users, module: "admin_users"
   namespace :admin do
+    get 'homes/top'
     resources :items, only:[:new, :index, :show, :edit, :create, :update]
     resources :genres, only:[:index, :create, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
+    resources :order, only:[:show, :update]
+    resources :order_details, only:[:update]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
